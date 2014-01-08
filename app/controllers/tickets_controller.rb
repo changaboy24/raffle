d29392173fbfb03d05226781d8e17a88dcb8edde
Rich Chang <changaboy24@gmail.com>
2014-01-07 20:52:40 -0600

    Create and update

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,5 +1,6 @@
 class TicketsController < ApplicationController
 	def new
+		@ticket = Ticket.new
 	end
 
 	def index
@@ -8,8 +9,25 @@ class TicketsController < ApplicationController
 
 	def create
 		@ticket = Ticket.new(post_params)
-		@ticket.save
-		redirect_to @ticket
+		if @ticket.save
+		  redirect_to @ticket
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+		@ticket = Ticket.find(params[:id])
+	end
+	
+	def update
+		@ticket = Ticket.find(params[:id])
+		if params.has_key?(:ticket)
+			@ticket.update(params[:ticket].permit(:name))
+    		redirect_to @ticket
+	  	else
+	    	render 'edit'
+	  	end
 	end
 
 	def show
